c9f8d675e108bc288a1579c741cb88f66f41df22
Tito Harditya W <tito.w@students.amikom.ac.id>
2021-01-14 16:22:45 +0700

    Update adapter_form_visitor.php

--- adapter_form_visitor.php
+++ adapter_form_visitor.php
@@ -1,15 +1,17 @@
 <?php
     include 'connection.php';
 
+    session_start();
+
     $no_id = $_POST["no_id"];
     $name = $_POST["name"];
     $phone=$_POST["phone"];
     $email=$_POST["email"];
     $visit_date=$_POST["visit_date"];
     $tujuan=$_POST["tujuan"];
-    $covid_form=$_POST['covid_form'];
+    $chck=$_POST['covid_form'];
 
-    $sql = "insert into tabel_visitor (no_id,name,phone,email,visit_date,tujuan,covid_form) values ('$no_id','$name','$phone','$email','$visit_date','$tujuan','$covid_form')";
+    $sql = "insert into tabel_visitor (no_id,name,phone,email,visit_date,tujuan,covid_form) values ('$no_id','$name','$phone','$email','$visit_date','$tujuan','$chck')";
 
     $hasil = mysqli_query($kon,$sql);
 
